e504fc17751ca21ef951d4e36f68250921745e2f
James Green <jgreenop22@gmail.com>
2026-04-15 12:53:31 -0700

    Solar Panels work in space properly lol

--- src/main/java/shipwrights/genesis_ad_astra/mixin/SolarPanelBlockEntityMixin.java
+++ src/main/java/shipwrights/genesis_ad_astra/mixin/SolarPanelBlockEntityMixin.java
@@ -1,23 +1,30 @@
 package shipwrights.genesis_ad_astra.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import earth.terrarium.adastra.common.blockentities.machines.SolarPanelBlockEntity;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import shipwrights.genesis.GenesisMod;
 
 @Mixin(value = SolarPanelBlockEntity.class, remap = false)
-public class SolarPanelBlockEntityMixin {
+public class SolarPanelBlockEntityMixin extends BlockEntity {
 
-    @WrapOperation(method = "serverTick", at = @At(value = "INVOKE", target = "Learth/terrarium/adastra/common/blockentities/machines/SolarPanelBlockEntity;isDay()Z"))
-    public boolean isDayWrap(SolarPanelBlockEntity instance, Operation<Boolean> original) {
-        Level level = instance.getLevel();
+    public SolarPanelBlockEntityMixin(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
+        super(p_155228_, p_155229_, p_155230_);
+    }
+
+    @WrapMethod(method = "isDay()Z")
+    public boolean isDayWrap(Operation<Boolean> original) {
+        Level level = this.level;
         if (level != null && GenesisMod.isSpaceDimension(level)) {
             return true;
         }
 
-        return original.call(instance);
+        return original.call();
     }
 }
